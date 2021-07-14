a07b778e6cb665d40108f37c6a1ed4358bb412da
claudio.siervi <claudio.siervi@gmail.com>
2021-07-14 01:03:13 -0300

    adiciona imagem sobreposta por coluna de botões

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -2,45 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: "Contador de pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Pessoas: 0",
-            style:
-                TextStyle(color: Colors.white30, fontWeight: FontWeight.bold),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(40.0),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "+1",
-                    style: TextStyle(color: Colors.white54, fontSize: 40.0),
+    title: "Contador de pessoas",
+    home: Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(40.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "-1",
-                    style: TextStyle(color: Colors.white54, fontSize: 40.0),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "-1",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Text(
-            "Pode entrar!",
-            style:
-                TextStyle(color: Colors.white54, fontWeight: FontWeight.w100),
-          ),
-        ],
-      )));
+              ],
+            ),
+            Text(
+              "Pode entrar!",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w100),
+            ),
+          ],
+        )
+      ],
+    ),
+  ));
 }
