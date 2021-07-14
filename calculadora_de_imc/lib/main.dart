00eeb889e4aaa80ea4bdeaf1de10a9b82bf46730
claudio.siervi <claudio.siervi@gmail.com>
2021-07-14 02:16:26 -0300

    adiciona coluna com textField e ElevatedButton

--- calculadora_de_imc/lib/main.dart
+++ calculadora_de_imc/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
-
-
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -19,12 +17,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title:Text("Calculadora de IMC") ,
+        title: Text("Calculadora de IMC"),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.refresh_sharp))
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.refresh_sharp)),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(
+            Icons.person_outline_rounded,
+            size: 40.0,
+            color: Colors.green,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (m)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          Container(height: 50.0,
+          child:  ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Calcular",
+              style: TextStyle(color: Colors.white, fontSize: 25.0),),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.purple,
+            ),
+          ),)
+
         ],
       ),
     );
