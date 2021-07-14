faf6d153443d394251403f6dbce561f6a4368ad5
claudio.siervi <claudio.siervi@gmail.com>
2021-07-14 00:53:51 -0300

    adiciona espaçamento entre botões contadores

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -4,8 +4,13 @@ void main() {
   runApp(MaterialApp(
       title: "Contador de pessoas",
       home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Pessoas: 0"),
+          Text(
+            "Pessoas: 0",
+            style:
+                TextStyle(color: Colors.white30, fontWeight: FontWeight.bold),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -13,18 +18,29 @@ void main() {
                 padding: EdgeInsets.all(40.0),
                 child: TextButton(
                   onPressed: () {},
-                  child: Text("+1"),
+                  child: Text(
+                    "+1",
+                    style: TextStyle(color: Colors.white54, fontSize: 40.0),
+                  ),
                 ),
               ),
               Padding(
                 padding: EdgeInsets.all(10.0),
                 child: TextButton(
                   onPressed: () {},
-                  child: Text("-1"),
+                  child: Text(
+                    "-1",
+                    style: TextStyle(color: Colors.white54, fontSize: 40.0),
+                  ),
                 ),
-              )
+              ),
             ],
-          )
+          ),
+          Text(
+            "Pode entrar!",
+            style:
+                TextStyle(color: Colors.white54, fontWeight: FontWeight.w100),
+          ),
         ],
       )));
 }
