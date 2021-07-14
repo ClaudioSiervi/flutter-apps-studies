83332229264ba8e1821c2afa3c8852a83d8ce4b7
claudio.siervi <claudio.siervi@gmail.com>
2021-07-14 02:23:39 -0300

    adiciona scrow com SingleChildScrollView

--- calculadora_de_imc/lib/main.dart
+++ calculadora_de_imc/lib/main.dart
@@ -26,42 +26,55 @@ class _HomeState extends State<Home> {
           IconButton(onPressed: () {}, icon: Icon(Icons.refresh_sharp)),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person_outline_rounded,
-            size: 40.0,
-            color: Colors.green,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Peso (kg)",
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Altura (m)",
-                labelStyle: TextStyle(color: Colors.green)),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          Container(height: 50.0,
-          child:  ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "Calcular",
-              style: TextStyle(color: Colors.white, fontSize: 25.0),),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.purple,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.person_outline_rounded,
+              size:150.0,
+              color: Colors.green,
             ),
-          ),)
-
-        ],
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (m)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            )
+          ],
+        ),
       ),
     );
   }
